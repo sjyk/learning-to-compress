54bcd569cb8faa176488b4d067fe7911061fdf14
Sanjay Krishnan <sanjaykrishn@gmail.com>
2021-10-22 14:50:51 -0500

    Updated with all the code

--- l2c/imcompress.py
+++ l2c/imcompress.py
@@ -41,9 +41,86 @@ class LinearAutoregressiveIM():
 			data[mask, j] = coderange + 1
 
 			#if it makes a difference
-			if entropy(data[:, j]) > entropy(src[:, j]):
-				data[mask, j] = src[mask, j]
-			else:
-				models.append(np.hstack([reg.intercept_.reshape(-1,1),reg.coef_]))
+			#if entropy(data[:, j]) > entropy(src[:, j]):
+			#	data[mask, j] = src[mask, j]
+			#else:
+			models.append(np.hstack([reg.intercept_.reshape(-1,1),reg.coef_]))
 
 		return data, models
+
+
+
+	def compress(self, src):
+		data = src.copy()
+		N,p = data.shape
+		coderange = np.max(src)
+		models = []
+		classes = []
+		
+		for j in range(1, p):
+			slice = list(range(0,j))
+			prediction = j
+
+
+			X = src[:,slice]
+			Y = src[:,prediction]
+
+			value,counts = np.unique(src[:,prediction], return_counts=True)
+			median_count = np.median(counts)
+			lookup = {v:(c < median_count)*1.0 for v,c in zip(value,counts)}
+
+
+			reg = LogisticRegression(class_weight=lookup)
+			reg.fit(X,Y)
+			Ypred = reg.predict(X)
+
+			mask = (Ypred.reshape(-1) == Y.reshape(-1))
+
+			data[mask, j] = coderange + 1
+
+			#is it worth it?
+			#cost of saving parameters
+			cost_estimate = j * 32 * len(reg.classes_) #bits
+			savings = (entropy(src[:, j]) - entropy(data[:, j]))*N
+
+			print('Model Cost Estimate (bits)', cost_estimate, 'Projected Savings (bits)', savings)
+
+			#if it makes a difference
+			if savings  <= cost_estimate:
+				data[mask, j] = src[mask, j]
+				models.append(None)
+				classes.append(None)
+			else:
+				models.append(np.hstack([reg.intercept_.reshape(-1,1),reg.coef_]))
+				classes.append(reg.classes_)
+
+			#print(reg.classes_.shape, reg.coef_.shape, reg.intercept_.shape)
+
+		return data, models, classes
+
+
+	def decompress(self, src, models, classes):
+
+		coderange = np.max(src)
+		N,p = src.shape
+
+		#left to right decoding
+		for (j,(m,c)) in enumerate(zip(models, classes)):
+			if not m is None:
+				slice = list(range(0,j))
+				X = src[:,slice]
+
+				reg = LogisticRegression()
+				reg.intercept_ = m[:,0]
+				reg.coef_ = m[:,1:]
+				#print(c, m.shape, c.shape)
+				reg.classes_ = c
+
+				Ypred = reg.predict(X)
+
+				#Ypred = np.array([classes[Ypred[i]] for i in range(N)])#assign actual class values
+
+				mask = (src[:,j] == coderange)
+				src[mask,j] = Ypred[mask]#impute
+
+		return src, np.max(src)
--- l2c/quarc.py
+++ l2c/quarc.py
@@ -4,6 +4,8 @@ from timeit import default_timer as timer
 import math
 from core import *
 from imcompress import *
+import glob
+import pickle
 
 
 #Quantized Autoregressive Codes (Quarc)
@@ -22,7 +24,8 @@ class Quarc(CompressionAlgorithm):
 
 		self.coderange = int(math.ceil(1.0/error_thresh))
 
-		self.AR_MODELS = target + '/learned_'
+		self.AR_MODELS = target + '/learned'
+		self.AR_CLASSES = target + '/classes'
 
 
 	"""The main compression loop
@@ -38,17 +41,23 @@ class Quarc(CompressionAlgorithm):
 
 
 		lin = LinearAutoregressiveIM()
-		codes, models = lin.compress(codes)
+		codes, models, classes = lin.compress(codes)
 		for i, model in enumerate(models):
-			fname = self.AR_MODELS + str(i)
+
+			if model is None:
+				continue
+
+			fname = self.AR_MODELS + '_' + str(i+1)
 			np.save(fname, model)
 			compressz(fname +'.npy', fname+'.gz')
 			self.DATA_FILES += [fname+'.gz']
 
-
 		struct = iarray_bitpacking(codes, order='F')
 		struct.flushz(self.CODES)
 		struct.flushmeta(self.METADATA)
+
+		classes.insert(0,None)
+		pickle.dump(classes, open(self.AR_CLASSES, 'wb'))
 
 		self.compression_stats['compression_latency'] = timer() - start
 		self.compression_stats['compressed_size'] = self.getSize()
@@ -71,6 +80,21 @@ class Quarc(CompressionAlgorithm):
 		p = int(P2 - 1)
 		N = int(normalization[0,p])
 		bit_length = struct.bit_length
+
+
+		#pass the parameters back to the decoder
+		lin = LinearAutoregressiveIM()
+		models = [None]*p
+		for f in glob.glob(self.AR_MODELS +'*'):
+			fname = f.split('.')[0]
+			decompressz(f, fname +'.npy')
+			model_index = int(fname.split('_')[1])
+			models[model_index] = np.load(fname +'.npy')
+
+		classes = pickle.load(open(self.AR_CLASSES,'rb'))
+
+		codes, coderange = lin.decompress(codes, models, classes)
+
 
 		for i in range(p):
 			codes[:,i] = (codes[:,i]/coderange + normalization[1,i])*(normalization[0,i] - normalization[1,i])
@@ -105,7 +129,7 @@ N,p = data.shape
 nn = Quarc('quantize')
 nn.load(data)
 nn.compress()
-#nn.decompress(data)
+nn.decompress(data)
 print(nn.compression_stats)
 
 
