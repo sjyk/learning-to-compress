aa87d538b91ed5e08ff76b70e0014839bf8b4c36
Sanjay Krishnan <sanjaykrishn@gmail.com>
2022-02-03 14:06:51 -0600

    Added with plotting code

--- l2c/experiment.py
+++ l2c/experiment.py
@@ -4,38 +4,117 @@ from itcompress import *
 from squish import *
 from delta import *
 from xor import *
+from spartan import *
+from apca import *
+import numpy as np
 
-ERROR_THRESH = 0.005
+def initialize(ERROR_THRESH = 0.005):
+	#set up baslines
+	BASELINES = []
+	BASELINES.append(IdentityGZ('gz', error_thresh=ERROR_THRESH))
+	BASELINES.append(Quantize('q', error_thresh=ERROR_THRESH))
+	BASELINES.append(QuantizeGZ('q+gz', error_thresh=ERROR_THRESH))
+	BASELINES.append(ItCompress('itcmp', error_thresh=ERROR_THRESH))
+	BASELINES.append(Spartan('sptn', error_thresh=ERROR_THRESH))
+	BASELINES.append(EntropyCoding('q+ent', error_thresh=ERROR_THRESH))
+	BASELINES.append(Sprintz('spz', error_thresh=ERROR_THRESH))
+	BASELINES.append(SprintzGzip('spz+gz', error_thresh=ERROR_THRESH))
+	BASELINES.append(Gorilla('grla', error_thresh=ERROR_THRESH))
+	BASELINES.append(GorillaLossy('grla+l', error_thresh=ERROR_THRESH))
+	BASELINES.append(AdaptivePiecewiseConstant('apca', error_thresh=ERROR_THRESH))
+	return BASELINES
 
-#set up baslines
-BASELINES = []
-BASELINES.append(Identity('none', error_thresh=ERROR_THRESH))
-BASELINES.append(IdentityGZ('gz', error_thresh=ERROR_THRESH))
-BASELINES.append(BitStripGZ('bs+gz', error_thresh=ERROR_THRESH))
-BASELINES.append(Quantize('q', error_thresh=ERROR_THRESH))
-BASELINES.append(QuantizeGZ('q+gz', error_thresh=ERROR_THRESH))
-BASELINES.append(ItCompress('itcmp', error_thresh=ERROR_THRESH))
-BASELINES.append(EntropyCoding('q+ent', error_thresh=ERROR_THRESH))
-BASELINES.append(Sprintz('spz', error_thresh=ERROR_THRESH))
-BASELINES.append(SprintzGzip('spz+gz', error_thresh=ERROR_THRESH))
-BASELINES.append(Gorilla('grla', error_thresh=ERROR_THRESH))
+def run(BASELINES,\
+		DATA_DIRECTORY = '/Users/sanjaykrishnan/Downloads/test_comp/', \
+		FILENAME = 'ColorHistogram.asc',\
+		N=1000):
+	data = np.loadtxt(DATA_DIRECTORY + FILENAME)[:N,1:]
 
-data = np.loadtxt('/Users/sanjaykrishnan/Downloads/test_comp/ColorHistogram.asc')[:100,1:]
+	bresults = {}
+	for nn in BASELINES:
+		nn.load(data)
+		nn.compress()
+		nn.decompress(data)
+		bresults[nn.target] = nn.compression_stats
 
-results = {}
-for nn in BASELINES:
-	nn.load(data)
-	nn.compress()
-	nn.decompress(data)
+	return bresults
 
-	results[nn.target] = nn.compression_stats
+
 
 
 
 #plotting
 
 import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
+plt.rcParams["figure.figsize"] = (10,4)
 
-plt.bar([k for k in results], [results[k]['compressed_size'] for k in results])
-plt.show()
+
+BASELINES = initialize()
+FILENAME = 'ColorHistogram.asc'
+SIZE_LIMIT = 1000
+bresults = run(BASELINES, N=SIZE_LIMIT)
+
+
+#compressed size
+plt.figure()
+plt.title(FILENAME.split('.')[0] + ": Compression Ratio" )
+plt.ylabel('Compression Ratio')
+plt.bar([k for k in bresults], [bresults[k]['compressed_ratio'] for k in bresults])
+plt.bar([k for k in bresults], [bresults[k].get('model_size',0)/bresults[k].get('original_size') for k in bresults])
+plt.legend(['Compression Ratio', 'Model Contribution'])
+plt.savefig('compression_ratio_' + FILENAME.split('.')[0]+'.png')
+
+
+
+#compression throughput (subtract bitpacking time)
+plt.figure()
+plt.title(FILENAME.split('.')[0] + ": Throughput" )
+plt.ylabel('Thpt bytes/sec')
+
+x1 = [i - 0.1 for i,_ in enumerate(bresults)]
+x2 = [i + 0.1 for i,_ in enumerate(bresults)]
+x = [i for i,_ in enumerate(bresults)]
+labels = [k for _,k in enumerate(bresults)]
+
+plt.bar(x1, [bresults[k]['original_size']/(bresults[k]['compression_latency'] - bresults[k].get('bitpacktime',0))  for k in bresults], width=0.2)
+plt.bar(x2, [bresults[k]['compressed_size']/(bresults[k]['decompression_latency'])  for k in bresults], width=0.2)
+
+plt.xticks(x,labels)
+plt.legend(['Compression', 'Decompression'])
+plt.yscale('log')
+plt.yticks([10000,100000,1000000, 10000000,1e8])
+plt.savefig('compression_tpt_' + FILENAME.split('.')[0]+'.png')
+
+
+#compression curves
+plt.figure()
+results = {}
+for error_thresh in range(7,0,-1):
+	BASELINES = initialize(ERROR_THRESH=10**(-error_thresh))
+	output = run(BASELINES, N=SIZE_LIMIT)
+	for k in output:
+
+		if not k in results:
+			results[k] = [None]*7
+
+		results[k][ (7-error_thresh) ] = output[k]
+
+
+ax = plt.axes()
+ax.set_prop_cycle('color',[plt.cm.tab20(i) for i in np.linspace(0, 1, len(results))])
+
+for technique in results:
+	rgb = np.random.rand(3,)
+	plt.plot([v['compressed_ratio'] for v in results[technique]],'s-')
+
+plt.legend([technique for technique in results])
+plt.xticks(list(range(0,7)),[ "1e-"+str(r) for r in range(7,0,-1)])
+plt.xlim([0,7])
+plt.xlabel('Error Threshold %')
+plt.title(FILENAME.split('.')[0] + ": Error Dependence" )
+plt.ylabel('Compression Ratio')
+plt.savefig('thresh_v_ratio' + FILENAME.split('.')[0]+'.png')
+
+
 
